c9c40117fbad99811eb376be163d77ee85a12df3
tariqabusada <tariqesa101@gmail.com>
2022-10-12 09:01:31 +0200

    fix: database url cases

--- src/config/connection.ts
+++ src/config/connection.ts
@@ -1,14 +1,29 @@
 import { Sequelize } from 'sequelize';
 require('dotenv').config();
+const { DB_URL, NODE_ENV, DB_URL_DEV, DB_URL_TEST } = process.env;
 
 let dbUrl;
 
-if (process.env.NODE_ENV === 'test') {
-  dbUrl = process.env.DB_URL;
-} else {
-  dbUrl = process.env.DB_URL;
+switch (NODE_ENV) {
+  case 'production': dbUrl = DB_URL;
+    break;
+  case 'development': dbUrl = DB_URL_DEV;
+    break;
+  case 'test': dbUrl = DB_URL_TEST;
+    break;
+  default: throw Error('there is error in database url');
 }
 
-const sequelize = new Sequelize(dbUrl, { logging: false });
+const sequelize = new Sequelize(dbUrl, {
+  logging: false,
+  dialectOptions: {
+    ssl: NODE_ENV === 'production'
+      ? {
+          require: true,
+          rejectUnauthorized: false
+        }
+      : false
+  }
+});
 
 export default sequelize;
