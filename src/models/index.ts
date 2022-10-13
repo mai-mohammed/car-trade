d0f8dd6a30d8c8415961b8adfe14b69053fb8842
Mai Mohammed <maymohammedabuabed@gmail.com>
2022-10-13 09:58:38 +0300

    fix: edit model name in src/models/index.ts

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,14 +1,14 @@
 import Image from './image';
 import Car from './car';
-import customers from './customer';
+import Customer from './customer';
 import Admin from './admin';
 
 Car.hasMany(Image);
 Image.belongsTo(Car);
 
-customers.hasMany(Car);
-Car.belongsTo(customers);
+Customer.hasMany(Car);
+Car.belongsTo(Customer);
 
 export default {
-  Image, Car, customers, Admin,
+  Image, Car, Customer, Admin,
 };
