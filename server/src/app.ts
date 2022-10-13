eee05bb877886a1d3a3d3bef8b3a3a29b2bb874b
husamkamal <husamkmal94@gmail.com>
2022-10-13 15:13:49 +0300

    ci: middleware handel clint error , middleware handle server error

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,26 @@
-import express, { Express, Request, Response } from 'express';
+import express, {
+  Express,
+  Request,
+  Response,
+  NextFunction,
+  ErrorRequestHandler,
+} from 'express';
+
+import createError from 'http-errors';
 
 const app:Express = express();
 
-app.get('/', (req:Request, res:Response) => {
-  res.send('hello world');
+app.get('/', (req:Request, res:Response, next: NextFunction) => {
+  next(createError(401, 'Please login to view this page.'));
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use('*', (req: Request, res: Response, next: NextFunction) => {
+  res.status(40).json('{ createError }');
+});
+// eslint-disable-next-line n/handle-callback-err, @typescript-eslint/no-unused-vars
+app.use('*', (error:ErrorRequestHandler, req:Request, res:Response, next:NextFunction) => {
+  res.status(500).json('internal server error');
 });
 
 export default app;
