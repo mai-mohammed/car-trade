1f6b6db1b556277c92ecd10cd60c65d947ce0a65
husamkamal <husamkmal94@gmail.com>
2022-10-16 16:02:10 +0300

    fix: images errors

---
+++ client/src/assets/index.ts
@@ -0,0 +1,32 @@
+import privateSeller from './service-1.svg';
+import inspected from './service-2.svg';
+import rating from './service-3.svg';
+import lamborghini from './lamborghini.png';
+import nissan from './nissan.png';
+import toyota from './toyota.png';
+import mitsbishi from './mitsubishi_v2.png';
+import audi from './audi.png';
+
+interface Image {
+  privateSeller: string,
+  inspected: string,
+  rating: string,
+  lamborghini: string,
+  nissan: string,
+  toyota: string,
+  mitsbishi: string,
+  audi: string,
+}
+
+const Images:Image = {
+  privateSeller,
+  inspected,
+  rating,
+  lamborghini,
+  nissan,
+  toyota,
+  mitsbishi,
+  audi,
+};
+
+export default Images;
