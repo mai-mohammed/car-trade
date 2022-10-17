4639c714ae0ae253ffb6ca630e379332e124b4cb
husamkamal <husamkmal94@gmail.com>
2022-10-17 11:17:04 +0300

    fix: handleError fun

--- client/src/services/axiosCongif.ts
+++ client/src/services/axiosCongif.ts
@@ -4,34 +4,17 @@ function handleError(error: AxiosError) {
   if (axios.isCancel(error)) {
     return 'Axios is canceled';
   }
-  const { response } = error;
-  switch (response?.status) {
-    case 400:
-      return {
-        message: `${response.status}: Bad request`,
-        description: response?.statusText ?? 'Unexpected error',
-      };
-    case 500:
-      return {
-        message: `${response.status}: Internal server busy`,
-        description: response?.statusText ?? 'Unexpected error',
-      };
-    default:
-      return {
-        message: `Error Code: ${response?.status ?? response?.status}`,
-        description: response?.statusText ?? 'Unexpected error',
-      };
-  }
+  const { response }:AxiosError = error;
+  const { status, data }:any = response;
+  return { status, data };
 }
+
 export const cancelTokenSource = axios.CancelToken.source();
 const httpInstance = axios.create({
   timeout: 5000,
   baseURL: 'https://localhost.com',
 });
 
-httpInstance.defaults.headers.common.isLoading = true;
-httpInstance.defaults.headers.common.successAlert = false;
-httpInstance.defaults.headers.common.errorAlert = true;
 Object.setPrototypeOf(httpInstance, axios);
 
 httpInstance.interceptors.request.use(async (config) => {
@@ -41,7 +24,7 @@ httpInstance.interceptors.request.use(async (config) => {
 });
 
 httpInstance.interceptors.response.use(
-  (res) => res,
+  (res) => res.data,
   (error) => {
     handleError(error);
     return Promise.reject(error);
