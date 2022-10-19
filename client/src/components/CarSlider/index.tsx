8c8a983e897de6390cfc1341a29794cb958443ef
husamkamal <husamkmal94@gmail.com>
2022-10-19 09:53:57 +0300

    fix: change slider

--- client/src/components/CarSlider/index.tsx
+++ client/src/components/CarSlider/index.tsx
@@ -1,47 +1,86 @@
+import SwiperCore, { EffectCoverflow, Navigation, Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import {
-  Navigation, A11y,
-} from 'swiper';
+import 'swiper/swiper-bundle.min.css';
+import 'swiper/swiper.min.css';
+import './style.css';
 import 'swiper/css';
 import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
 
+SwiperCore.use([EffectCoverflow, Pagination, Navigation]);
+
+const slideImg = [
+  {
+    id: 1,
+    image: 'https://swiperjs.com/demos/images/nature-1.jpg',
+  },
+  {
+    id: 2,
+    image: 'https://swiperjs.com/demos/images/nature-2.jpg',
+  },
+  {
+    id: 3,
+    image: 'https://swiperjs.com/demos/images/nature-3.jpg',
+  },
+  {
+    id: 4,
+    image: 'https://swiperjs.com/demos/images/nature-4.jpg',
+  },
+  {
+    id: 5,
+    image: 'https://swiperjs.com/demos/images/nature-5.jpg',
+  },
+  {
+    id: 6,
+    image: 'https://swiperjs.com/demos/images/nature-6.jpg',
+  },
+  {
+    id: 7,
+    image: 'https://swiperjs.com/demos/images/nature-7.jpg',
+  },
+  {
+    id: 8,
+    image: 'https://swiperjs.com/demos/images/nature-8.jpg',
+  },
+  {
+    id: 9,
+    image: 'https://swiperjs.com/demos/images/nature-9.jpg',
+  },
+
+];
 function CarSlider() {
   return (
-    <Swiper
-      modules={[Navigation, A11y]}
-      spaceBetween={10}
-      slidesPerView={3}
-      navigation
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-    >
-      <SwiperSlide>
-        <img
-          src="https://images.carswitch.com/331753nissan/1740762051796217.jpg?fit=crop&w=414&h=310&auto=format,compress&sat=30&vib=10&q=46"
-          alt="car"
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img
-          src="https://images.carswitch.com/331753nissan/1740762051796217.jpg?fit=crop&w=414&h=310&auto=format,compress&sat=30&vib=10&q=46"
-          alt="car"
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img
-          src="https://images.carswitch.com/331753nissan/1740762051796217.jpg?fit=crop&w=414&h=310&auto=format,compress&sat=30&vib=10&q=46"
-          alt="car"
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img
-          src="https://images.carswitch.com/331753nissan/1740762051796217.jpg?fit=crop&w=414&h=310&auto=format,compress&sat=30&vib=10&q=46"
-          alt="car"
-        />
-      </SwiperSlide>
-    </Swiper>
+    <div className="main-swiper">
+      <Swiper
+        effect="coverflow"
+        // autoplay={{
+        //   delay: 2500,
+        //   disableOnInteraction: false,
+        // }}
+        navigation
+        // grabCursor
+        // centeredSlides
+        // slidesPerView="auto"
+        spaceBetween={0}
+        slidesPerView={3}
+        coverflowEffect={{
+          // rotate: 50,
+          scale: 0.9,
+          // opacity: 0.5,
+          stretch: 5,
+          depth: 50,
+          modifier: 1,
+        }}
+        pagination
+        className="mySwiper"
+      >
+        {/* using array */}
+        {slideImg.map((img) => (
+          <SwiperSlide key={img.id}>
+            <img src={img.image} alt="" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 }
 
