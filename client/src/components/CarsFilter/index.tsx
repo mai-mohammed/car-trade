d6516c143719bf34a48568b65830d91fd592bf96
tariqabusada <tariqesa101@gmail.com>
2022-10-19 14:25:23 +0200

    fix: delete cars array

--- client/src/components/CarsFilter/index.tsx
+++ client/src/components/CarsFilter/index.tsx
@@ -34,103 +34,6 @@ const mileRang = [
   },
 ];
 
-const cars = [
-  {
-    brand: 'Toyota',
-    model: 'Land Cruiser VXR 4.0L V6',
-    price: 48000,
-    year: 2022,
-    mileage: 36000,
-    quality: 95,
-    isGoodPrice: false,
-    location: 'palestine - gaza',
-    state: 'on-market',
-    transmission: 'auto',
-    features: [
-      'Rear entertainment screens',
-      'Parking sensors',
-      'Cruise Control',
-      'Steering wheel control',
-      'Navigation',
-      'Bluetooth',
-      'Brake assist',
-    ],
-    description: 'This car had an accedent in the right side but the quality of the body generally is good',
-    fuel: 'diesel',
-    customerId: 1,
-  },
-  {
-    brand: 'BMW',
-    model: 'X6 M - Sport',
-    price: 55000,
-    year: 2021,
-    mileage: 21000,
-    quality: 85,
-    isGoodPrice: true,
-    location: 'palestine - ramallah',
-    state: 'on-market',
-    transmission: 'manual',
-    features: [
-      'Reversing Camera',
-      'Parking sensors',
-      'Collision warning system',
-      'Navigation',
-    ],
-    description: 'This car had an accedent in the left side but the quality of the body generally is good',
-    fuel: 'petrol',
-    customerId: 2,
-  },
-  {
-    brand: 'Mercedes',
-    model: 'GLC300 2.0L I4 TC',
-    price: 58000,
-    year: 2021,
-    mileage: 15800,
-    quality: 90,
-    isGoodPrice: true,
-    location: 'palestine - alnazreth',
-    state: 'on-market',
-    transmission: 'auto',
-    features: [
-      'Rear mirror camera',
-      'Brake assist',
-      'Reversing Camera',
-      'Collision warning system',
-      'Navigation',
-    ],
-    description: 'This car had an accedent in the left side but the quality of the body generally is good',
-    fuel: 'petrol',
-    customerId: 3,
-  },
-  {
-    brand: 'Infiniti',
-    model: 'QX70 3.7L V6',
-    price: 16500,
-    year: 2015,
-    mileage: 191000,
-    quality: 70,
-    isGoodPrice: false,
-    location: 'palestine - gaza',
-    state: 'under-check',
-    transmission: 'auto',
-    features: [
-      'Navigation',
-      'Bluetooth',
-      'Brake assist',
-      'Collision warning system',
-      'Auto dimming mirror',
-      'Headsup display',
-      'Automated Parking',
-      'Adaptive cruis control',
-    ],
-    description: '',
-    fuel: 'petrol',
-    customerId: 2,
-  },
-];
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-
 const getYears = ():Array<string> => {
   const yearsArr = [];
   const yearNow = new Date().getFullYear();
@@ -144,7 +47,7 @@ const getYears = ():Array<string> => {
 function CarsFilter() {
   const { state } = useLocation();
 
-  const [brand, setBrand] = useState(state.brand);
+  const [brand, setBrand] = useState(state?.brand || '');
   const [model, setModel] = useState('');
   const [mileage, setMileage] = useState(100000);
   const [year, setYear] = useState(null);
@@ -178,7 +81,6 @@ function CarsFilter() {
 
   useEffect(() => {
     // here we will fetch the data and set it on a props that it defined in parent component
-    console.log([...cars]);
   }, [brand, model, mileage, year, fuel, isGoodPrice]);
 
   return (
@@ -190,7 +92,7 @@ function CarsFilter() {
           id="combo-box"
           onChange={changeBrand}
           options={brands.map((e) => e.brand)}
-          defaultValue={state.brand}
+          defaultValue={state?.brand || ''}
           sx={{ width: 250 }}
           renderInput={(params) => (
             <TextField
