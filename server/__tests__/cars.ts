93e45194a276de5c17cbf0e2fb008b316a502e2f
abdou059 <abdoumonuir9@gmail.com>
2022-10-20 12:01:19 +0300

    fix: modify car details

--- server/__tests__/cars.ts
+++ server/__tests__/cars.ts
@@ -95,3 +95,11 @@ describe('/cars endpoint', () => {
     expect(result.statusCode).toEqual(200);
   });
 });
+describe('/cars/:id', () => {
+  test('should return all cars details when there is no filter car', async () => {
+    const result = await request(app).get('/api/v1/cars/2');
+    expect(result.body.msg).toEqual('done!');
+    expect(result.body.data[0].id).toEqual(2);
+    expect(result.body.data[0].brand).toEqual('BMW');
+  });
+});
