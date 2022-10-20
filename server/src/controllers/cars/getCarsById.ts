14e4c93cb700a12f075b3dbfaa34ad76aecce2d2
abdou059 <abdoumonuir9@gmail.com>
2022-10-20 11:46:23 +0300

    fix: modify car details

--- server/src/controllers/cars/getCarsById.ts
+++ server/src/controllers/cars/getCarsById.ts
@@ -1,9 +1,13 @@
 import { Request } from 'express';
+import * as yup from 'yup';
 import { getCarInfo } from '../../services';
 
 const getCarsById = async (req: Request) => {
   const { id } = req.params;
-
+  const schema = yup.object({
+    id: yup.number().integer().required(),
+  });
+  await schema.validate({ id });
   const result = await getCarInfo(id);
 
   return { status: 200, msg: 'done!', data: result };
