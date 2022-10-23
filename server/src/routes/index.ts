0abc1fb79d50e11f110371c93e7721f4a4124d16
Mai Mohammed <maymohammedabuabed@gmail.com>
2022-10-23 15:05:39 +0300

    fix: delete auth middleware test

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,9 @@
 import { Router, Request, Response } from 'express';
 import carsRouter from './cars';
-import authMiddleware from '../middlewares/authMiddleware';
 
 const router = Router();
 
-router.get('/', authMiddleware('admin'), (req: Request, res: Response) => {
+router.get('/', (req: Request, res: Response) => {
   res.send('hello');
 });
 
