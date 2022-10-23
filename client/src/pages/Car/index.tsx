b4d1e8a06125e858108f27e7a307e9161d0b1fae
tariqabusada <tariqesa101@gmail.com>
2022-10-23 11:39:53 +0200

    revert: the changes that added by mai

--- client/src/pages/Car/index.tsx
+++ client/src/pages/Car/index.tsx
@@ -3,6 +3,7 @@ import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import { useState } from 'react';
 
+import CarInfo from '../../components/CarInfoSection';
 import CarSlider from '../../components/CarSlider';
 import StripeForm from '../../components/StripForm';
 
@@ -17,9 +18,39 @@ function Car() {
   const options = {
     clientSecret: 'pi_1DseH42eZvKYlo2C5UQDyYph_secret_gowsU3j2SgDfFECrHNzE8UtGK',
   };
+
+  const testInfo = {
+    id: 1,
+    brand: 'Toyota',
+    model: 'Land Cruiser VXR 4.0L V6',
+    price: 48000,
+    year: 2022,
+    mileage: 36000,
+    quality: 95,
+    isGoodPrice: true,
+    location: 'palestine - gaza',
+    state: 'on-market',
+    transmission: 'auto',
+    features: [
+      'Rear entertainment screens',
+      'Parking sensors',
+      'Cruise Control',
+      'Steering wheel control',
+      'Navigation',
+      'Bluetooth',
+      'Brake assist',
+    ],
+    description: '',
+    fuel: 'diesel',
+    createdAt: '2022-10-17T08:26:03.560Z',
+    updatedAt: '2022-10-17T08:26:03.560Z',
+    customerId: 1,
+  };
+
   return (
     <>
       <CarSlider />
+      <CarInfo carInfo={testInfo} />
       <Button onClick={handleOpen}>Buy</Button>
       <Modal
         open={open}
