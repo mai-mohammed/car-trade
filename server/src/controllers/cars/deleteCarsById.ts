b465c9b85516f8ef0e5bff7cd5d9816d97bab234
abdou059 <abdoumonuir9@gmail.com>
2022-10-24 13:52:33 +0300

    fix: delete cars

--- server/src/controllers/cars/deleteCarsById.ts
+++ server/src/controllers/cars/deleteCarsById.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { Request } from 'express';
 import * as yup from 'yup';
 import createError from 'http-errors';
 import { deleteCars } from '../../services';
