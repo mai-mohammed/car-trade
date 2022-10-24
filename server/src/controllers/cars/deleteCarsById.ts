25a2d4b777ab5e9ef379172086f42c3584e2b038
abdou059 <abdoumonuir9@gmail.com>
2022-10-24 13:15:29 +0300

    fix: delete data

--- server/src/controllers/cars/deleteCarsById.ts
+++ server/src/controllers/cars/deleteCarsById.ts
@@ -9,9 +9,7 @@ const deleteCarsById = async (req:Request, res:Response) => {
   const { id } = req.params;
   await schema.validate({ id });
   const result = await deleteCars(id);
-  // console.log(result, 'sss');
-  // eslint-disable-next-line eqeqeq
-  if (result == 0) {
+  if (result === 0) {
     res.json({ msg: 'not found' });
   }
   return { status: 200, msg: 'done!', data: result };
