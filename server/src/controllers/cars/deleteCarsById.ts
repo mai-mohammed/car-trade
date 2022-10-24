2e8d5128f2b388ea536732bac5ca29c44fc91293
abdou059 <abdoumonuir9@gmail.com>
2022-10-24 13:40:43 +0300

    fix: delete cars

--- server/src/controllers/cars/deleteCarsById.ts
+++ server/src/controllers/cars/deleteCarsById.ts
@@ -1,16 +1,17 @@
 import { Request, Response } from 'express';
 import * as yup from 'yup';
+import createError from 'http-errors';
 import { deleteCars } from '../../services';
 
 const schema = yup.object({
   id: yup.number().integer().required(),
 });
-const deleteCarsById = async (req:Request, res:Response) => {
+const deleteCarsById = async (req:Request) => {
   const { id } = req.params;
   await schema.validate({ id });
   const result = await deleteCars(id);
   if (result === 0) {
-    res.json({ msg: 'not found' });
+    throw createError(400, 'car not found');
   }
   return { status: 200, msg: 'done!', data: result };
 };
