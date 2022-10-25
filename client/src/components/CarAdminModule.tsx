18c8375b0706662e44a4aad1b67ff2647ccdd755
husamkamal <husamkmal94@gmail.com>
2022-10-25 14:50:27 +0300

    build: create send request admin module sepper Relates #92

---
+++ client/src/components/CarAdminModule.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import { Typography } from '@mui/material';
+import CustomStepper from './stepper';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  borderRadius: '20px',
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function CarAdminModel() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Button
+        sx={{ marginRight: '0.5rem' }}
+        variant="contained"
+        color="success"
+        onClick={handleOpen}
+      >
+        Accept
+
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography sx={{ textAlign: 'center' }} component="h2">
+            Car selling Details
+            <hr style={{
+              margin: '1rem 0rem',
+              height: '.3rem',
+              width: '20rem',
+              backgroundColor: '#0A20E6',
+            }}
+            />
+          </Typography>
+          <CustomStepper />
+        </Box>
+      </Modal>
+    </div>
+  );
+}
