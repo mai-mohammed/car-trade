bbd933b7411f0437e8465a7c7c52513345260c81
abdou059 <abdoumonuir9@gmail.com>
2022-10-25 14:38:20 +0300

    fix: details car

--- server/src/routes/cars.ts
+++ server/src/routes/cars.ts
@@ -1,25 +1,15 @@
 import { Router } from 'express';
 import ExpressWrapper from './ExpressWrapper';
-<<<<<<< HEAD
-import {
-  getFilteredCars, getCarsById, updateCars, getCarsDetails,
-} from '../controllers';
-=======
->>>>>>> main
 import authMiddleware from '../middlewares/authMiddleware';
 import {
-  getFilteredCars, getCarsById, updateCar, deleteCarsById,
+  getFilteredCars, getCarsById, updateCar, deleteCarsById, getCarsDetails,
 } from '../controllers';
 
 const carsRouter = Router();
 
 carsRouter.get('/', ExpressWrapper(getFilteredCars));
 carsRouter.get('/', ExpressWrapper(getCarsById));
-<<<<<<< HEAD
-carsRouter.put('/:id', authMiddleware('admin'), ExpressWrapper(updateCars));
 carsRouter.get('/admin/cars', ExpressWrapper(getCarsDetails));
-=======
 carsRouter.delete('/:id', authMiddleware('admin'), ExpressWrapper(deleteCarsById));
 carsRouter.put('/:id', authMiddleware('admin'), ExpressWrapper(updateCar));
->>>>>>> main
 export default carsRouter;
