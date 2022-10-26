c7a10997607dd6d9038d5526262a9ae66979a45c
husamkamal <husamkmal94@gmail.com>
2022-10-26 10:42:14 +0300

    fix: change stepper content palce

--- client/src/components/stepper.tsx
+++ client/src/components/stepper.tsx
@@ -6,8 +6,6 @@ import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 import Button from '@mui/material/Button';
 import { Typography } from '@mui/material';
-import SellCarModal from './sendRequsetModel/Form';
-
 
 export default function CustomStepper() {
   const steps = [{ label: 'Car Into', component: 'first' }, { label: 'Car Image', component: 'second' }];
