fdfb8592e681502bd51a1712a271185f46baf767
abdou059 <abdoumonuir9@gmail.com>
2022-10-26 16:45:41 +0300

    fix: post delete

--- server/src/validation/signupSchema.ts
+++ server/src/validation/signupSchema.ts
@@ -3,12 +3,18 @@ import * as Yup from 'yup';
 const signupSchema = Yup.object({
   email: Yup
     .string()
-    .email('Enter a valid email')
+    .email('enter a valid email')
     .required('Email is required'),
+  fullName: Yup
+    .string()
+    .required('name is required'),
   password: Yup
     .string()
     .required('password is required')
     .min(8, 'Password should be of minimum 8 characters length'),
+  phoneNumber: Yup
+    .number()
+    .required('phone number required'),
 });
 
 export default signupSchema;
