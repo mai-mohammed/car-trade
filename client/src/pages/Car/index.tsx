88ed240a37cfcf0e6ce3a45713e96bfb2f5c3dc4
husamkamal <husamkmal94@gmail.com>
2022-10-27 08:41:12 +0300

    fix: add handle error if fetching reject

--- client/src/pages/Car/index.tsx
+++ client/src/pages/Car/index.tsx
@@ -7,23 +7,59 @@ import CarSlider from '../../components/CarSlider';
 import httpInstance from '../../services/axiosCongif';
 import { CarWithImages } from '../../interfaces';
 import CarNotFound from '../../components/CarNotFound';
+import CustomizedSnackbars from '../../components/snackbar';
 
 function Car() {
   const [carInfo, setCarInfo] = useState<CarWithImages>();
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [openSnackBar, setOpenSnackBar] = useState<boolean>(false);
+
+  const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpenSnackBar(false);
+  };
   const { id } = useParams();
   useEffect(() => {
     const getCarInfo = async () => {
-      setIsLoading(true);
-      const response = await httpInstance.get(`/cars/car/${id}`);
-      setCarInfo(response.data[0]);
-      setIsLoading(false);
-      // eslint-disable-next-line no-empty
+      try {
+        setIsLoading(true);
+        const response = await httpInstance.get(`/cars/car/${id}`);
+        setCarInfo(response.data[0]);
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+        setOpenSnackBar(true);
+      }
     };
     getCarInfo();
   }, [id]);
-  if (isLoading) return <CircularProgress />;
-  if (!carInfo) return <CarNotFound />;
+  if (isLoading) {
+    return (
+      <CircularProgress sx={{
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        width: '48px',
+        height: '50rem',
+      }}
+      />
+    );
+  }
+  if (!carInfo) {
+    return (
+      <>
+        <CarNotFound />
+        <CustomizedSnackbars
+          err="something went wrong"
+          open={openSnackBar}
+          handleClose={handleClose}
+        />
+      </>
+    );
+  }
   return (
     <>
       <CarSlider carInfo={carInfo} />
