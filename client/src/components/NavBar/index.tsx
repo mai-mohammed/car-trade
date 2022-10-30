c307749e76e160c970a28c2ca1ff1be957734b5a
Mai Mohammed <maymohammedabuabed@gmail.com>
2022-10-30 17:26:07 +0300

    build: handle admin cases + handle changes requested

--- client/src/components/NavBar/index.tsx
+++ client/src/components/NavBar/index.tsx
@@ -1,27 +1,17 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState, useContext } from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
+import {
+  AppBar, Toolbar, Box, IconButton, Typography,
+  Menu, Container, Avatar, Button, Tooltip, MenuItem,
+} from '@mui/material';
+
 import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
-import Link from '@mui/material/Link';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, NavLink } from 'react-router-dom';
 import { UserContext } from '../../context';
 import { UserContextTypeWithDispatch } from '../../interfaces';
 import httpInstance from '../../services/axiosConfig';
 import CustomizedSnackbars from '../snackbar';
-
-const pages = ['HOME', 'HOW IT WORK', 'SHOP', 'CONTACT'];
-const settings = ['Profile', 'Logout'];
 
 function NavBar() {
   const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
@@ -29,6 +19,14 @@ function NavBar() {
   const { userInfo, setUserInfo }:UserContextTypeWithDispatch = useContext(UserContext);
   const [snackBarProperties, setSnackBarProperties] = useState<
   { open:boolean, message:string, type:'success' | 'error' }>({ open: false, message: '', type: 'error' });
+
+  const pages = userInfo?.role.toLowerCase() === 'admin'
+    ? [{ title: 'HOME', path: '' }, { title: 'SHOP', path: 'cars' }, { title: 'Dashboard', path: 'admin' }]
+    : [{ title: 'HOME', path: '' }, { title: 'SHOP', path: 'cars' }, { title: 'Contact', path: '' }];
+
+  const settings = userInfo?.role.toLowerCase() === 'admin'
+    ? [{ title: 'Logout', path: 'logout' }]
+    : [{ title: 'Profile', path: 'profile' }, { title: 'Logout', path: 'logout' }];
 
   const Navigate = useNavigate();
 
@@ -63,10 +61,10 @@ function NavBar() {
         } catch (err) {
           setSnackBarProperties({ open: true, message: 'something went wrong! Try again.', type: 'error' });
         }
+        setUserInfo(null);
+        Navigate('/');
       };
       logout();
-      setUserInfo(null);
-      Navigate('/');
     }
   };
   return (
@@ -134,8 +132,8 @@ function NavBar() {
               }}
             >
               {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
+                <MenuItem key={page.title} onClick={handleCloseNavMenu}>
+                  <NavLink to={page.path}>{page.title}</NavLink>
                 </MenuItem>
               ))}
             </Menu>
@@ -163,9 +161,9 @@ function NavBar() {
 
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, marginLeft: '2rem' }}>
             {pages.map((page) => (
-              <Link key={`link${page}`} href="/" sx={{ textDecoration: 'none' }}>
+              <NavLink key={`link${page.title}`} to={page.path} style={{ textDecoration: 'none' }}>
                 <Button
-                  key={page}
+                  key={page.title}
                   component="button"
                   onClick={handleCloseNavMenu}
                   sx={{
@@ -177,14 +175,45 @@ function NavBar() {
                     fontSize: '14px',
                   }}
                 >
-                  {page}
+                  {page.title}
                 </Button>
-              </Link>
+              </NavLink>
             ))}
           </Box>
 
-          { userInfo !== null
+          {(userInfo === null)
             ? (
+              <Box sx={{ flexGrow: 0 }}>
+                <NavLink to="login" style={{ textDecoration: 'none' }}>
+                  <Button
+                    variant="text"
+                    sx={{
+                      color: 'var(--text-color)',
+                    }}
+                  >
+                    Login
+
+                  </Button>
+                </NavLink>
+                <NavLink to="signup" style={{ textDecoration: 'none' }}>
+                  <Button
+                    variant="outlined"
+                    sx={{
+                      borderColor: 'var(--text-color)',
+                      color: 'var(--text-color)',
+                      ':hover': {
+                        backgroundColor: '#2f36430f',
+                        borderColor: 'var(--text-color)',
+                      },
+                    }}
+                  >
+                    SingUp
+
+                  </Button>
+                </NavLink>
+              </Box>
+            )
+            : (
               <Box sx={{ flexGrow: 0, display: 'flex', alignItems: 'center' }}>
                 <Tooltip title="Open settings">
                   <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
@@ -216,43 +245,20 @@ function NavBar() {
                   onClose={handleCloseUserMenu}
                 >
                   {settings.map((setting) => (
-                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                      <Typography textAlign="center" onClick={() => handleSetting(setting)}>{setting}</Typography>
+                    <MenuItem key={setting.title} onClick={handleCloseUserMenu}>
+                      <Typography
+                        textAlign="center"
+                        onClick={() => handleSetting(setting.title)}
+                      >
+                        {setting.title}
+
+                      </Typography>
                     </MenuItem>
                   ))}
                 </Menu>
               </Box>
-            ) : (
-              <Box sx={{ flexGrow: 0 }}>
-                <Link href="/login" sx={{ textDecoration: 'none' }}>
-                  <Button
-                    variant="text"
-                    sx={{
-                      color: 'var(--text-color)',
-                    }}
-                  >
-                    Login
-
-                  </Button>
-                </Link>
-                <Link href="signup" sx={{ textDecoration: 'none' }}>
-                  <Button
-                    variant="outlined"
-                    sx={{
-                      borderColor: 'var(--text-color)',
-                      color: 'var(--text-color)',
-                      ':hover': {
-                        backgroundColor: '#2f36430f',
-                        borderColor: 'var(--text-color)',
-                      },
-                    }}
-                  >
-                    SingUp
-
-                  </Button>
-                </Link>
-              </Box>
             ) }
+
         </Toolbar>
       </Container>
       <CustomizedSnackbars
--- client/src/context.tsx
+++ client/src/context.tsx
@@ -1,6 +1,8 @@
 import {
   createContext, useEffect, useMemo, useState,
 } from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
 import { UserContextType, UserContextTypeWithDispatch } from './interfaces';
 import httpInstance from './services/axiosConfig';
 
@@ -15,14 +17,17 @@ export const UserContext = createContext<UserContextTypeWithDispatch>({
 
 export default function UserInfoProvider({ children }:any) {
   const [userInfo, setUserInfo] = useState<UserContextType | null>(null);
-
+  const [loading, setLoading] = useState<boolean>(false);
   useEffect(() => {
     const getUserInfo = async () => {
       try {
+        setLoading(true);
         const result = await httpInstance.get('/auth/user');
         setUserInfo(result.data);
+        setLoading(false);
       } catch (error) {
         setUserInfo(null);
+        setLoading(false);
       }
     };
     getUserInfo();
@@ -32,6 +37,16 @@ export default function UserInfoProvider({ children }:any) {
     userInfo, setUserInfo,
   }), [userInfo]);
 
+  if (loading) {
+    return (
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    );
+  }
   return (
     <UserContext.Provider value={value}>
       { children }
