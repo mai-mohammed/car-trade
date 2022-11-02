7af3f52cfd122304515acd743e4dca354c4740db
Mai Mohammed <maymohammedabuabed@gmail.com>
2022-11-02 15:58:38 +0300

    fix: add final email when the car add to the market

--- server/src/controllers/cars.ts
+++ server/src/controllers/cars.ts
@@ -280,12 +280,13 @@ const updateCars = async (req: Request) => {
   const { id } = req.params;
   await updateCarSchema.validate(body);
   const result = await updateCarServes(body, id);
+  const subject = 'Car trade team';
+  let content;
+  const userInfo:{ email: string, fullName: string } = await findUserById(
+    { id: result[1][0].customerId },
+  );
   if (body.state === 'under-check') {
-    const userInfo:{ email: string, fullName: string } = await findUserById(
-      { id: result[1][0].customerId },
-    );
-    const subject = 'Car trade team';
-    const content = `<!DOCTYPE html>
+    content = `<!DOCTYPE html>
     <html xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office" lang="en">
     
     <head>
@@ -455,8 +456,178 @@ const updateCars = async (req: Request) => {
     </body>
     
     </html>`;
-    await sendEmail(userInfo, subject, content);
+  } else if (body.state === 'on-market') {
+    content = `<!DOCTYPE html>
+     <html xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office" lang="en">
+     
+     <head>
+       <title></title>
+       <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <!--[if mso]><xml><o:OfficeDocumentSettings><o:PixelsPerInch>96</o:PixelsPerInch><o:AllowPNG/></o:OfficeDocumentSettings></xml><![endif]-->
+       <!--[if !mso]><!-->
+       <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet" type="text/css">
+       <!--<![endif]-->
+       <style>
+         * {
+           box-sizing: border-box;
+         }
+     
+         body {
+           margin: 0;
+           padding: 0;
+         }
+     
+         a[x-apple-data-detectors] {
+           color: inherit !important;
+           text-decoration: inherit !important;
+         }
+     
+         #MessageViewBody a {
+           color: inherit;
+           text-decoration: none;
+         }
+     
+         p {
+           line-height: inherit
+         }
+     
+         .desktop_hide,
+         .desktop_hide table {
+           mso-hide: all;
+           display: none;
+           max-height: 0px;
+           overflow: hidden;
+         }
+     
+         @media (max-width:520px) {
+           .row-content {
+             width: 100% !important;
+           }
+     
+           .mobile_hide {
+             display: none;
+           }
+     
+           .stack .column {
+             width: 100%;
+             display: block;
+           }
+     
+           .mobile_hide {
+             min-height: 0;
+             max-height: 0;
+             max-width: 0;
+             overflow: hidden;
+             font-size: 0px;
+           }
+     
+           .desktop_hide,
+           .desktop_hide table {
+             display: table !important;
+             max-height: none !important;
+           }
+         }
+       </style>
+     </head>
+     
+     <body style="background-color: #FFFFFF; margin: 0; padding: 0; -webkit-text-size-adjust: none; text-size-adjust: none;">
+       <table class="nl-container" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #FFFFFF;">
+         <tbody>
+           <tr>
+             <td>
+               <table class="row row-1" align="center" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+                 <tbody>
+                   <tr>
+                     <td>
+                       <table class="row-content stack" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000000; width: 500px;" width="500">
+                         <tbody>
+                           <tr>
+                             <td class="column column-1" width="100%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 5px; padding-bottom: 5px; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;">
+                               <table class="heading_block block-1" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+                                 <tr>
+                                   <td class="pad" style="padding-top:15px;text-align:center;width:100%;">
+                                     <h1 style="margin: 0; color: #525760; direction: ltr; font-family: Arial, Helvetica Neue, Helvetica, sans-serif; font-size: 27px; font-weight: 700; letter-spacing: normal; line-height: 120%; text-align: center; margin-top: 0; margin-bottom: 0;"><span style="color: #000000;"><strong>Your Car Added To The Market !</strong></span></h1>
+                                   </td>
+                                 </tr>
+                               </table>
+                             </td>
+                           </tr>
+                         </tbody>
+                       </table>
+                     </td>
+                   </tr>
+                 </tbody>
+               </table>
+               <table class="row row-2" align="center" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+                 <tbody>
+                   <tr>
+                     <td>
+                       <table class="row-content stack" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000000; width: 500px;" width="500">
+                         <tbody>
+                           <tr>
+                             <td class="column column-1" width="100%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 5px; padding-bottom: 5px; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;">
+                               <table class="divider_block block-1" width="100%" border="0" cellpadding="10" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+                                 <tr>
+                                   <td class="pad">
+                                     <div class="alignment" align="center">
+                                       <table border="0" cellpadding="0" cellspacing="0" role="presentation" width="80%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+                                         <tr>
+                                           <td class="divider_inner" style="font-size: 1px; line-height: 1px; border-top: 2px solid #5666EA;"><span>&#8202;</span></td>
+                                         </tr>
+                                       </table>
+                                     </div>
+                                   </td>
+                                 </tr>
+                               </table>
+                               <table class="paragraph_block block-2" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;">
+                                 <tr>
+                                   <td class="pad" style="padding-top:15px;">
+                                     <div style="color:#2f3643;direction:ltr;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:16px;font-weight:400;letter-spacing:0px;line-height:150%;text-align:left;mso-line-height-alt:24px;">
+                                       <p style="margin: 0; margin-bottom: 13px;"><strong>Dear ${userInfo.fullName},</strong></p>
+                                       <p style="margin: 0;">We are happy to tell you that your sell car request has been accepted and your car added to the market finally🌟.</p>
+                                     </div>
+                                   </td>
+                                 </tr>
+                               </table>
+                               <table class="paragraph_block block-3" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;">
+                                 <tr>
+                                   <td class="pad" style="padding-top:15px;">
+                                     <div style="color:#2f3643;direction:ltr;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:16px;font-weight:400;letter-spacing:0px;line-height:150%;text-align:left;mso-line-height-alt:24px;">
+                                       <p style="margin: 0; margin-bottom: 13px;">you can track the state of your request through your email or your profile.</p>
+                                       <p style="margin: 0;">&nbsp;</p>
+                                     </div>
+                                   </td>
+                                 </tr>
+                               </table>
+                               <table class="button_block block-4" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+                                 <tr>
+                                   <td class="pad" style="text-align:center;">
+                                     <div class="alignment" align="center">
+                                       <!--[if mso]><v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" style="height:42px;width:133px;v-text-anchor:middle;" arcsize="10%" stroke="false" fillcolor="#5666ea"><w:anchorlock/><v:textbox inset="0px,0px,0px,0px"><center style="color:#ffffff; font-family:Arial, sans-serif; font-size:16px"><![endif]-->
+                                       <div style="text-decoration:none;display:inline-block;color:#ffffff;background-color:#5666ea;border-radius:4px;width:auto;border-top:0px solid transparent;font-weight:400;border-right:0px solid transparent;border-bottom:0px solid transparent;border-left:0px solid transparent;padding-top:5px;padding-bottom:5px;font-family:Arial, Helvetica Neue, Helvetica, sans-serif;text-align:center;mso-border-alt:none;word-break:keep-all;"><span style="padding-left:20px;padding-right:20px;font-size:16px;display:inline-block;letter-spacing:normal;"><span style="word-break: break-word; line-height: 32px;" dir="ltr">Go to Profile!</span></span></div>
+                                       <!--[if mso]></center></v:textbox></v:roundrect><![endif]-->
+                                     </div>
+                                   </td>
+                                 </tr>
+                               </table>
+                             </td>
+                           </tr>
+                         </tbody>
+                       </table>
+                     </td>
+                   </tr>
+                 </tbody>
+               </table>
+             </td>
+           </tr>
+         </tbody>
+       </table><!-- End -->
+     </body>
+     
+     </html>`;
   }
+  await sendEmail(userInfo, subject, content);
   return { status: 200, msg: 'done!', data: result };
 };
 
