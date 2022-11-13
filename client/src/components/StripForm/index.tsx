db7ab0bbb9333c9621348b893ca3afca8b21fb60
tariqabusada <tariqesa101@gmail.com>
2022-11-13 05:37:24 +0200

    fix: stripe validation on elements

--- client/src/components/StripForm/index.tsx
+++ client/src/components/StripForm/index.tsx
@@ -1,6 +1,6 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Button } from '@mui/material';
-import { PaymentElement } from '@stripe/react-stripe-js';
+import { PaymentElement, useElements, useStripe } from '@stripe/react-stripe-js';
 import { useParams } from 'react-router-dom';
 import { SnackBarContext } from '../../contexts';
 import httpInstance from '../../services';
@@ -9,8 +9,12 @@ import './style.css';
 import { SnackBarContextTypeWithDispatch } from '../../interfaces';
 
 function StripeForm() {
+  const stripe = useStripe();
+  const elements = useElements();
   const { id } = useParams();
   const { setSnackBarProperties }:SnackBarContextTypeWithDispatch = useContext(SnackBarContext);
+  const [isProcessing, setIsProcessing] = useState(false);
+
   const buyCar = async () => {
     try {
       await httpInstance.patch('/cars/buy', { id });
@@ -27,24 +31,47 @@ function StripeForm() {
       });
     }
   };
-  const handleClick = (e:React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
 
-    buyCar();
+    if (!stripe || !elements) {
+      return;
+    }
+
+    setIsProcessing(true);
+    const { error } = await stripe.confirmPayment({
+      elements,
+      redirect: 'if_required',
+    });
+
+    if (error?.type === 'card_error' || error?.type === 'validation_error') {
+      setSnackBarProperties({
+        type: 'error',
+        message: error?.message || '',
+        open: true,
+      });
+    } else {
+      buyCar();
+    }
+
+    setIsProcessing(false);
   };
 
   return (
-    <form className="stripe-form">
-      <PaymentElement />
+    <form className="stripe-form" onSubmit={handleSubmit}>
+      <PaymentElement id="payment-element" />
       {' '}
       <Button
-        onClick={(e) => handleClick(e)}
+        disabled={isProcessing || !stripe || !elements}
+        id="submit"
         sx={{ color: '#0A20E6', borderColor: '#0A20E6' }}
         className="pay-button"
         type="submit"
       >
-        Pay
-
+        <span id="button-text">
+          {isProcessing ? 'Processing ... ' : 'Pay now'}
+        </span>
       </Button>
     </form>
   );
